6c7f94b1f6a5c4464ef6bfbd5676ce2df786f54f
christian williams <christian.williamsj@gmail.com>
2019-09-04 06:28:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,8 @@ def speak_to_grandma(phrase)
   if  phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upercase
-    return "NO, NOT SINCE 1938!"
+    return ""
   else
-    return "HUH?! SPEAK UP, SONNY!"
+    return "
   end
 end  
