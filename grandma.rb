5973b0bf4c94925731e06dfb9626c8389e2cfc8d
christian williams <christian.williamsj@gmail.com>
2019-09-04 06:18:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(statement)
-  if 
-    return "I LOVE YOU TOO PUMPKIN!" 
+  if "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!" 
 end
 end
