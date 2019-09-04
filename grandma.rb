7bcd18418e30b9aafa5d1602500cbe77231daa94
christian williams <christian.williamsj@gmail.com>
2019-09-04 06:27:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,10 +12,10 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(statement)
-  if  statement == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if  phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif statement == statement.upercase
+  elsif phrase == phrase.upercase
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
