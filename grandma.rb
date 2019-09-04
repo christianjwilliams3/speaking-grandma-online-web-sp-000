8b25126e5e022353789cecfc8011fcde43a28dfd
christian williams <christian.williamsj@gmail.com>
2019-09-04 06:53:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(statement)
   elsif statement == statement
     return "HUH?! SPEAK UP, SONNY!"
        puts "HUH?! SPEAK UP, SONNY!"
-  else statement == string
+  else
     return "NO, NOT SINCE 1938!"
       puts "NO, NOT SINCE 1938!"
   end
