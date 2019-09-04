7ea7347848c81e4d0e38396080789cb81058e818
christian williams <christian.williamsj@gmail.com>
2019-09-04 07:03:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(statement)
   if statement == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif statement == uppercase.statement
+  elsif statement == 
     return "NO, NOT SINCE 1938!"
       puts "NO, NOT SINCE 1938!"
   else 
