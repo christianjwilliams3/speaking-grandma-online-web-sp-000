f0775b9e94c10552400c3d44fd451ceaa3dc3b38
christian williams <christian.williamsj@gmail.com>
2019-09-04 06:54:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,9 +17,9 @@ def speak_to_grandma(statement)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif statement == statement
     return "NO, NOT SINCE 1938!"
-       puts "HUH?! SPEAK UP, SONNY!"
+      puts "NO, NOT SINCE 1938!"
   else
-    return "NO, NOT SINCE 1938!"
+    return "HUH?! SPEAK UP, SONNY!"
       puts "HUH?! SPEAK UP, SONNY!"
   end
 end
